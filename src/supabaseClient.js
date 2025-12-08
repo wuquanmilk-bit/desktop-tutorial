c304ab1a7461ba1d2fea4923be0be66a2c5f9111
杭州全叔 <236676588+wuquanmilk-bit@users.noreply.github.com>
2025-12-08 21:56:23 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -12,8 +12,8 @@ const getConfig = () => {
   if (!envUrl || envUrl === '链接') {
     console.warn('⚠️ 环境变量VITE_SUPABASE_URL未正确设置，使用默认值')
     return {
-      url: 'https://umqmouebfogpsdcchiqk.supabase.co',
-      key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcW1vdWViZm9ncHNkY2NoaXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzQwNzIsImV4cCI6MjA4MDQxMDA3Mn0.5qsLQusjbP6sG3fweJUCYpz01IZsdxP3uNEETwytT-s'
+      url: 'https://zuplqpojcjwbmmjpacqx.supabase.co',
+      key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1cGxxcG9qY2p3Ym1tanBhY3F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxOTc2NjEsImV4cCI6MjA4MDc3MzY2MX0.efrwhQ_DBBix8HRymgNwfkosi64R8OotOO1keL3oK5o'
     }
   }
   
